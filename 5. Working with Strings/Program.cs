bcf23a1517bad5d41ec031493eb3285f7b443111
Dell-User <Dell-User@DESKTOP-H6E2ELM>
2024-01-22 15:08:01 +0200

    Add project files.

---
+++ 5. Working with Strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Working_with_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This will write same line");
+            Console.WriteLine("This will \n wrap to next line");
+            Console.WriteLine("This will \" print out the quotation mark.");
+
+            string firstName = "Anele";
+            string lastName = "Thombela";
+            Console.WriteLine(firstName + " " + lastName);  
+
+            Console.WriteLine(lastName.Length);
+
+            Console.WriteLine(firstName.ToUpper());
+
+            Console.WriteLine(lastName.Contains("Fu"));
+
+            Console.WriteLine(firstName[0]);
+
+            Console.WriteLine("index of a is ");
+            Console.WriteLine(lastName.IndexOf('a'));
+
+            Console.WriteLine(firstName.Substring(1));
+            Console.WriteLine(firstName.Substring(1,2));
+            Console.Read();
+
+
+        }
+    }
+}
